fa03acc2efcf30bdd57ee249b503b87dfcca39fc
Daniel Wyb <danielwyb@icloud.com>
2018-02-27 01:11:27 -0500

    Fix missing slash

--- components/HeaderTags.js
+++ components/HeaderTags.js
@@ -5,7 +5,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link rel="icon" type="image/png" href="./static/splash-favicon.png">
+      <link rel="icon" type="image/png" href="./static/splash-favicon.png"/>
       <link rel="stylesheet" href="https://use.typekit.net/xob8rbo.css"/>
     </Head>
   </div>
