c8200621a225c165151d2a94b892ed3ff8cb3bf6
Daniel Wyb <danielwyb@icloud.com>
2018-03-18 15:09:13 -0400

    Privacy added

---
+++ pages/privacy.js
@@ -0,0 +1,340 @@
+import Layout from "../components/MainLayout"
+import Footer from "../components/Footer"
+export default () => (
+	<Layout>
+		<div className="privacy">
+			<h1>HEXA PRIVACY POLICY</h1>
+			<p>
+				This Privacy Policy describes how Hexa Financial Group Inc. ("Hexa")
+				collects, uses, stores, shares, and protects your information whenever
+				you use hexafinancial.com, splashwallet.io, a Hexa mobile app, any Hexa
+				API, or third party applications relying on such an API (the "Hexa
+				Site") or any Hexa services. By using the Hexa Site and Hexa services,
+				you consent to the data practices prescribed in this statement. We may
+				periodically post changes to this Privacy Policy on this page, and it is
+				your responsibility to review this Privacy Policy frequently and we
+				encourage you to visit this page often. When required by law, we will
+				notify you of any changes to this Privacy Policy.
+			</p>
+			<p>
+				<em>Last updated: March 17, 2018</em>
+			</p>
+			<div>
+				<h3>HOW WE COLLECT INFORMATION ABOUT YOU</h3>
+			</div>
+			<p>
+				When you use Hexa services, we collect information sent to us through
+				your computer, mobile phone, or other access device. This information
+				may include your IP address, device information including, but not
+				limited to, identifier, device name and type, operating system,
+				location, mobile network information, and standard web log information,
+				such as your browser type, traffic to and from our site, and the pages
+				you accessed on our website. Hexa does not intentionally collect
+				information from or about any individual who is under 13 years old.
+			</p>
+			<p>
+				If you create an account or use Hexa services, we, or our affiliates
+				vendors acting on our behalf may collect the following types of
+				information:
+			</p>
+			<ul>
+				<li>
+					Contact information - your name, address, phone, email, Skype ID, and
+					other similar information; and
+				</li>
+				<li>
+					Financial information - the full bank account and routing numbers
+					and/or credit card numbers that you link to your Hexa Account or input
+					when you use paid Hexa services. If you do not use the Hexa Conversion
+					Service, you may opt out of providing this information.
+				</li>
+			</ul>
+			<p>
+				If you seek permissions to raise Digital Currency buy and sell limits
+				associated with your Hexa Account, we may require you to provide
+				additional information which we may use in collaboration with service
+				providers acting on our behalf to verify your identity or address,
+				and/or to manage risk. This information may include your date of birth,
+				taxpayer or government identification number, a copy of your
+				government-issued identification, or other personal information. We may
+				also obtain information about you from third parties such as credit
+				bureaus and identity verification services.
+			</p>
+			<p>
+				When you use Hexa services, we collect information about your
+				transactions and/or your other activities on our website and we may
+				continuously collect information about your computer, mobile device, or
+				other access device for fraud prevention purposes, to monitor for
+				possible breach of your Hexa Account, and to identify any malicious
+				software or other activity that may harm Hexa or its users.
+			</p>
+			<p>
+				You may choose to provide us with access to certain personal information
+				stored by third parties such as social media sites (such as Facebook and
+				Twitter). The information we have access to varies by site and is
+				controlled by your privacy settings on that site and your authorization.
+				By associating an account managed by a third party with your Hexa
+				account and authorizing Hexa to have access to this information, you
+				agree that Hexa may collect, store, and use this information in
+				accordance with this Privacy Policy.
+			</p>
+			<p>
+				Finally, we may collect additional information you may disclose to our
+				customer support team.
+			</p>
+			<div>
+				<h3>HOW WE USE COOKIES</h3>
+			</div>
+			<p>
+				When you access our website or content or use our application or Hexa
+				services, we or companies we work with may place small data files called
+				cookies or pixel tags on your computer or other device. We use these
+				technologies to:
+			</p>
+			<ul>
+				<li>Recognize you as a Hexa customer;</li>
+				<li>Customize Hexa services, content, and advertising;</li>
+				<li>Measure promotional effectiveness; and</li>
+				<li>
+					Collect information about your computer or other access device to
+					mitigate risk, help prevent fraud, and promote trust and safety.
+				</li>
+			</ul>
+
+			<p>
+				We use both session and persistent cookies when you access our website
+				or content. Session cookies expire and no longer have any effect when
+				you log out of your account or close your browser. Persistent cookies
+				remain on your browser until you erase them or they expire.
+			</p>
+			<p>
+				We also use Local Shared Objects, commonly referred to as "Flash
+				cookies," to help ensure that your account security is not compromised,
+				to spot irregularities in behavior to help prevent fraud, and to support
+				our sites and services.
+			</p>
+			<p>
+				We encode our cookies so that only we can interpret the information
+				stored in them. You are free to decline our cookies if your browser or
+				browser add-on permits, but doing so may interfere with your use of Hexa
+				services. The help section of most browsers or browser add-ons provides
+				instructions on blocking, deleting, or disabling cookies.
+			</p>
+			<p>
+				You may encounter Hexa cookies or pixel tags on websites that we do not
+				control. For example, if you view a web page created by a third party or
+				use an application developed by a third party, there may be a cookie or
+				pixel tag placed by the web page or application. Likewise, these third
+				parties may place cookies or pixel tags that are not subject to our
+				control and the Hexa Privacy Policy does not cover their use.
+			</p>
+			<div>
+				<h3>HOW WE PROTECT AND STORE PERSONAL INFORMATION</h3>
+			</div>
+			<p>
+				Throughout this policy, we use the term "personal information" to
+				describe information that can be associated with a specific person and
+				can be used to identify that person. This Privacy Policy does not apply
+				to personal information that has been anonymized so that it does not and
+				cannot be used to identify a specific user. Hexa takes reasonable
+				precautions, as described herein, to protect your personal information
+				from loss, misuse, unauthorized access, disclosure, alteration, and
+				destruction.
+			</p>
+			<p>
+				We store and process your personal and transactional information,
+				including certain payment information, such as your encrypted bank
+				account and/or routing numbers, on our computers in the United States
+				and elsewhere in the world where Hexa facilities or our service
+				providers are located, and we protect it by maintaining physical,
+				electronic, and procedural safeguards in compliance with applicable U.S.
+				federal and state regulations. We use computer safeguards such as
+				firewalls and data encryption, we enforce physical access controls to
+				our buildings and files, and we authorize access to personal information
+				only for those employees who require it to fulfill their job
+				responsibilities.
+			</p>
+			<p>
+				We store our customers' personal information securely throughout the
+				life of the customer's Hexa Account. Hexa will retain your personal
+				information for a minimum of five years or as necessary to comply with
+				our legal obligations or to resolve disputes.
+			</p>
+			<div>
+				<h3>HOW WE USE THE PERSONAL INFORMATION WE COLLECT</h3>
+			</div>
+			<p>
+				Our primary purpose in collecting personal information is to provide you
+				with a secure, smooth, efficient, and customized experience. We may use
+				your personal information to:
+			</p>
+			<ul>
+				<li>Provide Hexa services and customer support you request;</li>
+				<li>Process transactions and send notices about your transactions;</li>
+				<li>Resolve disputes, collect fees, and troubleshoot problems;</li>
+				<li>
+					Prevent and investigate potentially prohibited or illegal activities,
+					and/or violations of our posted user terms;
+				</li>
+				<li>
+					Customize, measure, and improve Hexa services and the content and
+					layout of our website and applications;
+				</li>
+				<li>
+					Deliver targeted marketing, service update notices, and promotional
+					offers based on your communication preferences; and
+				</li>
+				<li>
+					Verify your identity by comparing your personal information against
+					third-party databases.
+				</li>
+			</ul>
+			<p>
+				We will not use your personal information for purposes other than those
+				purposes we have disclosed to you, without your permission. From time to
+				time we may request your permission to allow us to share your personal
+				information with third parties. You may opt out of having your personal
+				information shared with third parties, or from allowing us to use your
+				personal information for any purpose that is incompatible with the
+				purposes for which we originally collected it or subsequently obtained
+				your authorization. If you choose to so limit the use of your personal
+				information, certain features or Hexa services may not be available to
+				you.
+			</p>
+			<h3>HOW PERSONAL INFORMATION IS SHARED WITH OTHER HEXA USERS</h3>
+			<p>
+				If you use your Hexa Account to transfer Digital Currency in connection
+				with the purchase or sale of goods or services, we or you may also
+				provide the seller with your shipping address, name, and/or email to
+				help complete your transaction with the seller. The seller is not
+				allowed to use this information to market their services to you unless
+				you have agreed to it. If an attempt to transfer Digital Currency to
+				your seller fails or is later invalidated, we may also provide your
+				seller with details of the unsuccessful transfer. To facilitate dispute
+				resolutions, we may provide a buyer with the seller's address so that
+				goods can be returned to the seller.
+			</p>
+			<p>
+				In connection with a Digital Currency transfer between you and a third
+				party, including merchants, a third party may share information about
+				you with us, such as your email address or mobile phone number which may
+				be used to inform you that a transfer has been sent to or received from
+				the third party. We may use this information in connection with such
+				transfers to confirm that you are a Hexa customer, that Digital Currency
+				transfers are enabled, and/or to notify you that you have received
+				Digital Currency. If you request that we validate your status as a Hexa
+				customer with a third party, we will do so. You may also choose to send
+				Digital Currency to or request Digital Currency from an email address.
+				In such cases, your user name will be displayed in an email message
+				notifying the user of the designated email address of your action.
+				Please note that merchants you interact with may have their own privacy
+				policies, and Hexa is not responsible for their operations, including,
+				but not limited to, their information practices.
+			</p>
+			<p>
+				If you authorize one or more third-party applications to access your
+				Hexa account, then information you have provided to Hexa may be shared
+				with those third parties. Unless you provide further authorization,
+				these third parties are not allowed to use this information for any
+				purpose other than to facilitate your transactions using Hexa services.
+			</p>
+			<div>
+				<h3>HOW WE SHARE PERSONAL INFORMATION WITH OTHER PARTIES</h3>
+			</div>
+			<p>We may share your personal information with:</p>
+			<ul>
+				<li>
+					Third party identity verification services in order to prevent fraud.
+					This allows Hexa to confirm your identity by comparing the information
+					you provide us to public records and other third party databases.
+					These service providers may create derivative data based on your
+					personal information that can be used solely in connection with
+					provision of identity verification and fraud prevention services;
+				</li>
+				<li>
+					Service providers under contract who help with parts of our business
+					operations such as bill collection, marketing, and technology
+					services. Our contracts require these service providers to only use
+					your information in connection with the services they perform for us,
+					and prohibit them from selling your information to anyone else;
+				</li>
+				<li>Financial institutions with which we partner;</li>
+				<li>
+					Companies or other entities that we plan to merge with or be acquired
+					by. Should such a combination occur, we will require that the new
+					combined entity follow this Privacy Policy with respect to your
+					personal information. You will receive prior notice of any change in
+					applicable policy;
+				</li>
+				<li>
+					Companies or other entities that purchase Hexa assets pursuant to a
+					court-approved sale under U.S. Bankruptcy law;
+				</li>
+				<li>Law enforcement, and officials, or other third parties when:</li>
+				<li>
+					We are compelled to do so by a subpoena, court order, or similar legal
+					procedure; or
+				</li>
+				<li>
+					We believe in good faith that the disclosure of personal information
+					is necessary to prevent physical harm or financial loss, to report
+					suspected illegal activity or to investigate violations of our User
+					Agreement; and
+				</li>
+				<li>Other third parties with your consent or direction to do so.</li>
+			</ul>
+			<p>
+				Before Hexa shares your information with any third party that is not
+				acting as an agent to perform tasks on behalf of and under the
+				instructions of Hexa, Hexa will enter into a written agreement requiring
+				that the third party to provide at least the same level of privacy
+				protection as required hereunder.
+			</p>
+			<p>
+				If you establish a Hexa account indirectly on a third party website or
+				via a third party application, any information that you enter on that
+				website or application (and not directly on a Hexa website) will be
+				shared with the owner of the third party website or application and your
+				information may be subject to their privacy policies.
+			</p>
+			<p>
+				In general, we will notify you of material changes to this policy by
+				updating the last updated date at the top of this page, and we will
+				provide you with explicit notice of material changes as required by law.
+				We recommend that you visit this page frequently to check for changes.
+			</p>
+			<div>
+				<h3>HOW YOU CAN ACCESS OR CHANGE YOUR PERSONAL INFORMATION</h3>
+			</div>
+			<p>
+				You are entitled to review, correct, or amend your personal information,
+				or to delete that information where it is inaccurate, and you may do so
+				at any time by logging in to your account and clicking the Profile or My
+				Account tab. This right shall only be limited where the burden or
+				expense of providing access would be disproportionate to the risks to
+				your privacy in the case in question, or where the rights of persons
+				other than you would be violated. If you close your Hexa account, we
+				will mark your account in our database as "Closed," but will keep your
+				account information in our database for a period of time described
+				above. This is necessary in order to deter fraud, by ensuring that
+				persons who try to commit fraud will not be able to avoid detection
+				simply by closing their account and opening a new account. However, if
+				you close your account, your personally identifiable information will
+				not be used by us for any further purposes, nor sold or shared with
+				third parties, except as necessary to prevent fraud and assist law
+				enforcement, as required by law, or in accordance with this Privacy
+				Policy.
+			</p>
+		</div>
+		<Footer />
+		<style jsx>
+			{`
+				.privacy {
+					max-width: 800px;
+					line-height: 30px;
+					margin: 80px auto 100px;
+				}
+			`}
+		</style>
+	</Layout>
+)
