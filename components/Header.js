cda4beba61fb6aa41f435f018b369703b8b0ad8c
Daniel Wyb <danielwyb@icloud.com>
2018-03-22 07:04:08 -0400

    added padding to hero

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
-import Link from 'next/link'
-import Logo from './Logo'
+import Link from "next/link"
+import Logo from "./Logo"
 
 const Header = () => (
 	<div>
 		<Logo />
-		<a href="https://medium.com/splash-wallet/splash-lets-make-using-crypto-easy-25303b456a6b">Our story</a>
-		<style jsx>{`
-			div {
-				display: flex;
-				justify-content: space-between;
-				margin: 0 auto;
-				align-items: center;
-			}
+		<a href="https://medium.com/splash-wallet/splash-lets-make-using-crypto-easy-25303b456a6b">
+			Learn more
+		</a>
+		<style jsx>
+			{`
+				div {
+					display: flex;
+					justify-content: space-between;
+					margin: 12px auto 0;
+					align-items: center;
+				}
 			`}
 		</style>
 	</div>
